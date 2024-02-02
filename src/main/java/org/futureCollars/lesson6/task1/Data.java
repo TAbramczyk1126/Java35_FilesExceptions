776dfebacc90ed48ebf6d6ffd448e19745793823
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-02-02 22:42:17 +0100

    pom

---
+++ src/main/java/org/futureCollars/lesson6/task1/Data.java
@@ -0,0 +1,31 @@
+package org.futureCollars.lesson6.task1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Data {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("C:\\Tomek\\JAVA\\Java35_FilesExceptions\\src\\main\\resources\\data.txt");
+        Paths.get("C:\\Tomek\\JAVA\\Java35_FilesExceptions\\src\\main\\resources\\data.txt");
+
+        writeToFile(file, "Kurs");
+        writeToFile(file, "Java");
+        writeToFile(file, "Lekcja 6");
+        writeToFile(file, "Pliki");
+        writeToFile(file, "Wyjątki");
+        writeToFile(file, "Pliki");
+        writeToFile(file, "Koniec pliku");
+
+        System.out.println(Files.readString(file.toPath()));
+    }
+
+    public static void writeToFile(File file, String text) throws IOException {
+        String currentText = Files.readString(file.toPath());
+        String newText = currentText + text + System.lineSeparator();
+        Files.writeString(file.toPath(), newText);
+        currentText = newText;
+    }
+}
